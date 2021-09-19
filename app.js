d1e335c012539d3089a172705ac3c5cb13b2f2d8
devy-null <devynull@protonmail.com>
2021-09-19 21:58:28 +0200

    Fix

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  },
+  methods: {
+    greet: async function() {
+      send({ type: 'llOwnerSay', message: 'Hello world!' });
+    }
   }
 });
