56eac80cbd5082fcb154241c512e49dbfcc73feb
devy-null <devynull@protonmail.com>
2021-09-19 22:08:40 +0200

    Fix json error

--- server_sync.js
+++ server_sync.js
@@ -3,7 +3,8 @@ function uuidv4() {
 	return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
 }
 
-const base_data = JSON.parse(atob(document.location.hash.substring(1)));
+const base_data = JSON.parse(atob(document.location.hash.substring(1)) || "{}");
+
 let gitApp;
 
 async function getApps()
