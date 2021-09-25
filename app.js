f6630dc3416781fb035abbeacdf8c4593408b58d
devy-null <devynull@protonmail.com>
2021-09-25 16:26:31 +0200

    Messages

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    name: 'Mee',
+    messages: []
+  },
+  methods: {
+    sendMessage: function(text) {
+      send({ type: 'broadcast', payload: { type: 'message', author: 'A', message: text } })
+    }
   }
+});
+
+document.addEventListener('broadcast', (event) => {
+  let detail = event.detail,
+      message = detail.data;
+
+    console.log('A', message);
 });
