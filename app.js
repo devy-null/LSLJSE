2418dffd46c921d507b27dbb5bc74600a1667d20
cnifes <cnifes@gmail.com>
2021-09-25 22:00:08 +0200

    Work

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 var app = new Vue({
   el: '#app',
   data: {
-    name: 'Mee',
+    name: '',
+    message: '',
     messages: []
   },
+  created() {
+    document.addEventListener('broadcast', (event) => {
+      if (event.detail.type == 'message') {
+        this.messages.push({
+          author: event.detail.author,
+          text: event.detail.message
+        });
+    
+        console.log('A', event.detail);
+      }
+    });
+
+    send({ type: 'llGetDisplayName', key: base_data['avatar'] }).then(name => this.name = name);
+  },
   methods: {
-    sendMessage: function(text) {
-      send({ type: 'broadcast', payload: { type: 'message', author: 'A', message: text } })
+    sendMessage: function () {
+      send({ type: 'broadcast', payload: { type: 'message', author: this.name, message: this.message } });
+      this.message = '';
     }
   }
 });
 
-document.addEventListener('broadcast', (event) => {
-  let detail = event.detail,
-      message = detail.data;
-
-    console.log('A', message);
-});
